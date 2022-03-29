f11556e98a6bc41c2e10b02b10b4103aafd3e985
David L. Inman <davidlevere@gmail.com>
2022-03-29 17:45:23 -0400

    put tokenizer into main

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,15 @@ extern crate array_pattern;
 mod parsing;
 
 fn main() {
+
+    let input = "";
+    let token_result = parsing::tokenizer::tokenize(input);
+    let tokens = match token_result {
+        Ok(t) => t,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        },
+    };
     println!("Hello, world!");
 }
